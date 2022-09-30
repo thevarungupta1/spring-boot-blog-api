84f50eb30d202d6aa2f40a585a8cbd5eefe02f35
Varun Gupta <thevarungupta5@gmail.com>
2022-09-30 19:03:41 +0530

    Initial Commit

---
+++ src/main/java/com/thevarungupta/spring/boot/blog/api/SpringBootBlogApiApplication.java
@@ -0,0 +1,13 @@
+package com.thevarungupta.spring.boot.blog.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBlogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBlogApiApplication.class, args);
+	}
+
+}
